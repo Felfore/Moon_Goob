471de17bbe089d30ead4af8d7669df9f115a22d2
Mose <noreply@example.com>
2026-04-02 22:21:09 -0400

    Revert "move NoFootprintsComponent to common"
    This reverts commit 5f80321def0d611c27dcd61002087d423a56290c.

--- Content.Goobstation.Server/Footprints/NoFootprintsComponent.cs
+++ Content.Goobstation.Server/Footprints/NoFootprintsComponent.cs
@@ -4,7 +4,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-namespace Content.Goobstation.Common.Footprints;
+namespace Content.Goobstation.Server.Footprints;
 
 [RegisterComponent]
 public sealed partial class NoFootprintsComponent : Component;
